057b8a3b012ebd23cf836b4062716fefd7eb0c8c
rogeraabbccdd <rogeraabbccdd@gmail.com>
2019-10-15 21:21:29 +0800

    Add more informations
    - History low price from SteamDB
    - Current price from Steam
    - Fix links of steam bundle

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const linebot = require('linebot');
 const rp = require('request-promise');
 const schedule = require('node-schedule');
+const cloudscraper = require('cloudscraper');
 
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
@@ -19,16 +20,16 @@ const getItadPlainByName = (json, name) => {
   });
 }
 
-const getSteamIDByPlain = (json, plain) => {
+const getSteamInfoByPlain = (json, plain) => {
   let steam = json.data.list.filter((list)=>{
     return list.plain == plain && list.shop.id == 'steam';
   });
   if(steam.length > 0){
     let steamUrl = steam[0].urls.buy;
-    let id = steamUrl.match( /\/(app|sub|bundle|friendsthatplay|gamecards|recommended)\/([0-9]{1,7})/ );
-    return id ? parseInt( id[ 2 ], 10 ) : -1;
+    let info = steamUrl.match( /\/(app|sub|bundle|friendsthatplay|gamecards|recommended)\/([0-9]{1,7})/ );
+    return info ? {id: parseInt( info[ 2 ], 10 ), type: info[1] }: { id: -1, type: 'null'};
   }
-  else return -1;
+  else return {id: -1, type: 'null'};
 }
 
 const itadShops = 'amazonus,bundlestars,chrono,direct2drive,dlgamer,dreamgame,fireflower,gamebillet,gamejolt,gamersgate,gamesplanet,gog,humblestore,humblewidgets,impulse,indiegalastore,indiegamestand,itchio,macgamestore,newegg,origin,paradox,savemi,silagames,squenix,steam,uplay,wingamestore';
@@ -84,7 +85,8 @@ bot.on('message', function(event) {
         }
         else {
           let plain = find[0].plain;
-          let appId = getSteamIDByPlain(json, plain);
+          let appTitle = find[0].title;
+          let appInfo = getSteamInfoByPlain(json, plain);
 
           // get history best
           rp(`https://api.isthereanydeal.com/v01/game/lowest/?key=${process.env.ITAD_KEY}&plains=${plain}&region=us&country=US&shops=${itadShops}`)
@@ -96,34 +98,113 @@ bot.on('message', function(event) {
               rp(`https://api.isthereanydeal.com/v01/game/prices/?key=${process.env.ITAD_KEY}&plains=${plain}&region=us&country=US&shops=${itadShops}`)
                 .then((res)=>{
                   let current = JSON.parse(res).data[plain].list[0];
-                  let replyText = `${name}\n`+
-                                `原價: ${current.price_old} USD / ${Math.round(current.price_old*exRateUSDTW*100)/100} TWD\n`+
-                                `歷史最低: ${lowest.price} USD / ${Math.round(lowest.price*exRateUSDTW*100)/100} TWD, -${lowest.cut}%, ${lowestDate.toLocaleDateString()} 在 ${lowest.shop.name}\n`+
-                                `目前最低: ${current.price_new} USD / ${Math.round(current.price_new*exRateUSDTW*100)/100} TWD, -${current.price_cut}%, 在 ${current.shop.name}\n`+
-                                `${current.url}\n`+
-                                `更多資訊:\nhttps://isthereanydeal.com/game/${plain}/info/`;
+                  let replyTextDeal = `${appTitle}\n`+
+                                `.\n` +
+                                `IsThereAnyDeal:\n` +
+                                `原價: ${current.price_old} USD / ${Math.round(current.price_old*exRateUSDTW*100)/100} TWD\n` +
+                                `目前最低: ${current.price_new} USD / ${Math.round(current.price_new*exRateUSDTW*100)/100} TWD, -${current.price_cut}%, 在 ${current.shop.name}\n` +
+                                `歷史最低: ${lowest.price} USD / ${Math.round(lowest.price*exRateUSDTW*100)/100} TWD, -${lowest.cut}%, ${lowestDate.toLocaleDateString()} 在 ${lowest.shop.name}\n` +
+                                `${current.url}\n`;
                   
-                  // non steam
-                  if(appId == -1) event.reply(replyText);
+                  let replyTextSteam = '.\nSteam:\n';
 
+                  let replyTextInfo = '.\n' +
+                                    `更多資訊:\n` +
+                                    `https://isthereanydeal.com/game/${plain}/info/\n`;
+                  
                   // is steam
+                  if(appInfo.id != -1) {
+                    replyTextInfo += `https://store.steampowered.com/${appInfo.type}/${appInfo.id}/\n` + 
+                                    `https://steamdb.info/${appInfo.type}/${appInfo.id}/`;
+
+                    // get twd info
+                    if(appInfo.type == 'app'){
+                      let replyImage = {
+                          type: 'image',
+                          originalContentUrl: `https://steamcdn-a.akamaihd.net/steam/apps/${appInfo.id}/header.jpg`,
+                          previewImageUrl: `https://steamcdn-a.akamaihd.net/steam/apps/${appInfo.id}/header.jpg`
+                      };
+
+                      rp(`http://store.steampowered.com/api/appdetails/?appids=${appInfo.id}&cc=tw&filters=price_overview`)
+                        .then((res)=>{
+                          let appTWPrice = JSON.parse(res);
+                          
+                          // check ok
+                          if(appTWPrice[appInfo.id].success && typeof appTWPrice[appInfo.id].data.length != 'array') {
+                            let price_overview = appTWPrice[appInfo.id].data.price_overview;
+                            replyTextSteam += `目前價格: ${price_overview.final_formatted}, `+
+                                              `原價 ${price_overview.initial_formatted}, ` +
+                                              `-${price_overview.discount_percent}%`
+
+                            // check steamdb for history low
+                            cloudscraper.get(`https://steamdb.info/api/ExtensionGetPrice/?appid=${appInfo.id}&currency=TWD`)
+                              .then((res)=>{
+                                let json = JSON.parse(res);
+                                if(json.success) replyTextSteam += `歷史最低: ${json.data.lowest.price}, -${json.data.lowest.discount}%, ${new Date(json.data.lowest.date).toLocaleDateString()}`;
+                                else replyTextSteam += `歷史最低: SteamDB 查詢失敗`;
+
+                                event.reply([ replyImage, { type: 'text', text: replyTextDeal + replyTextSteam + replyTextInfo }]);
+                              })
+                              .catch((err)=>{
+                                console.log(err);
+                                replyTextSteam += `歷史最低: SteamDB 查詢失敗，請求被對方拒絕`;
+
+                                event.reply([ replyImage, { type: 'text', text: replyTextDeal + replyTextSteam + replyTextInfo }]);
+                              });
+                          }
+                          else {
+                            replyTextSteam += '目前價格: Steam 查詢失敗\n';
+                            
+                            event.reply([ replyImage, { type: 'text', text: replyTextDeal + replyTextSteam + replyTextInfo }]);
+                          }
+                        })
+                        .catch((err)=>{
+                          console.log(err);
+                          replyTextSteam += '目前價格: Steam 查詢失敗\n'
+                          event.reply([ replyImage, { type: 'text', text: replyTextDeal + replyTextSteam + replyTextInfo }]);
+                        })
+                    }
+                    else if(appInfo.type == 'sub'){
+                      rp(`https://store.steampowered.com/api/packagedetails/?packageids=${appInfo.id}&cc=tw`)
+                        .then((res)=>{
+                          let appTWPrice = JSON.parse(res);
+                          if(appTWPrice[appInfo.id].success) {
+                            let price = appTWPrice[appInfo.id].data.price
+                            replyTextSteam += `目前價格:  NT$ ${price.final/100}, `+
+                                                `原價:  NT$ ${price.initial/100}, ` +
+                                                `-${price.discount_percent}%` +
+                                                `單買原價:  NT$ ${price.individual/100}`;
+                            
+                            event.reply(replyTextDeal + replyTextSteam + replyTextInfo);
+                          }
+                          else {
+                            replyTextSteam += '目前價格: Steam 沒有提供這個組合包的資料\n'
+                            event.reply(replyTextDeal + replyTextSteam + replyTextInfo);
+                          }
+                        })
+                        .catch((err)=>{
+                          console.log(err);
+                          replyTextSteam += '目前價格: Steam 查詢失敗\n'
+                          event.reply(replyTextDeal + replyTextSteam + replyTextInfo);
+                        })
+                    }
+                    else {
+                      replyTextSteam += 'Steam 沒有提供這個組合包的資料\n'
+                      event.reply(replyTextDeal + replyTextSteam + replyTextInfo);
+                    }
+                  }
                   else {
-                      replyText += `\nhttps://steamdb.info/app/${appId}/`;
-                      event.reply([ {
-                        type: 'image',
-                        originalContentUrl: `https://steamcdn-a.akamaihd.net/steam/apps/${appId}/header.jpg`,
-                        previewImageUrl: `https://steamcdn-a.akamaihd.net/steam/apps/${appId}/header.jpg`
-                      },  { type: 'text', text: replyText }]);
+                    event.reply(replyTextDeal + replyTextInfo);
                   }
                 })
                 .catch((err)=>{
                   console.log(err);
-                  event.reply('目前最低價查詢失敗，請稍後再試');
+                  event.reply('IsThereAnyDeal 目前最低價查詢失敗，請稍後再試');
                 })
             })
             .catch((err)=>{
               console.log(err);
-              event.reply('歷史最低價查詢失敗，請稍後再試');
+              event.reply('IsThereAnyDeal 歷史最低價查詢失敗，請稍後再試');
             })
         }
       })
